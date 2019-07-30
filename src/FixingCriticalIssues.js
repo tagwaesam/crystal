c9a5f18c455559c11fd7f0ae9489b2268af6e34b
Tagwa-Esam <tagwaesam5@gmail.com>
2019-07-30 10:54:26 -0400

    Create FixingCriticalIssues.js
    add text

---
+++ src/FixingCriticalIssues.js
@@ -0,0 +1,31 @@
+
+
+import React, { Component } from 'react';
+
+
+
+
+class FixingCriticalIssues extends Component{
+	render() {
+		return (
+
+			<div className="fixIssuesContainer">
+
+				<h1>Fixing Critical Issues</h1>
+				<p>
+				• Crystal House has boldly transformed to create value for our customers
+				and shareholders.<br />
+				• With our newly energized focus on our two core business platforms — 
+				Energy Solutions and Commercial & Residential Solutions — we can confront
+				the challenges of an increasingly complex and unpredictable marketplace
+				from a position of strength, driving near- and long-term value as
+				a trusted partner for our customers.
+				</p>
+    	</div>
+
+	 	);}
+
+}
+
+
+export default FixingCriticalIssues;
