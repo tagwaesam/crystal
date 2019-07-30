b387f5f347222111cccd042549cbe3bb4bc2e71a
Tagwa-Esam <tagwaesam5@gmail.com>
2019-07-30 10:54:34 -0400

    Create ContactUs.js
    add text

---
+++ src/ContactUs.js
@@ -0,0 +1,32 @@
+
+import React, { Component } from 'react';
+
+
+
+
+class ContactUs extends Component{
+	render() {
+		return (
+
+			<div className="ContactContanier">
+
+				<h1>Contact us</h1>
+				<p>
+				<b>• Email</b><br /><br />
+				crystalhouseconsultants@gmail.com<br /><br />
+				<b>• Phone</b><br /><br />
+				+1 (203)285-9079<br /><br />
+				<b>• Fax</b><br /><br />
+				+1 (585)626-6893<br /><br />
+				<b>• Address</b><br /><br />
+				1050 Kelwyn ln<br /><br />
+				Lewisville, NC, 27023<br /><br />
+				</p>
+    	</div>
+
+	 	);}
+
+}
+
+
+export default ContactUs;
