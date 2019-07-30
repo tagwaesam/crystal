f29beb899988959d9e38dd9c0c2b3059ce462138
Tagwa-Esam <tagwaesam5@gmail.com>
2019-07-30 10:57:40 -0400

    Create OurValuesInAction.js
    add text

---
+++ src/OurValuesInAction.js
@@ -0,0 +1,30 @@
+
+
+import React, { Component } from 'react';
+
+
+
+
+class OurValuesInAction extends Component{
+	render() {
+		return (
+
+			<div className="valuesContainer">
+
+
+				<h1>Our Values in Action</h1>
+				<p>
+				 • Our teams across the world are constantly striving to be more connected,
+				 forward-looking and customer-focused.<br /><br />
+				 • Our company values serve as our foundation, informing every decision we make.<br /><br />
+				 • They are part of a shared vision that keeps us grounded as a company, moving
+				 forward together even as the industries we serve continue to shift and transform.
+				</p>
+    	</div>
+
+	 	);}
+
+}
+
+
+export default OurValuesInAction;
