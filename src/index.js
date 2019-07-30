ed9c10c6595a676f265b626bd14586a12a41e180
Tagwa-Esam <tagwaesam5@gmail.com>
2019-07-30 10:56:48 -0400

    Update index.js
    code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route,  BrowserRouter as Router, Switch } from 'react-router-dom'
 import './index.css';
-import App from './App';
+import './App.css';
+import App from './App.js';
+import NavComponent from './NavComponent.js'
+import OurStory from './OurStory.js'
+import OurValuesInAction from './OurValuesInAction.js'
+import FixingCriticalIssues from './FixingCriticalIssues.js'
+import CommercialAndResidentialSolutions from './CommercialAndResidentialSolutions.js'
+import Careers from './Careers.js'
+import ContactUs from './ContactUs.js'
+import Notfound from './Notfound.js'
+import Footer from './footerComponent.js'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const routing = (
+  <Router>
+    <div>
+      <NavComponent/>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/AboutUs" component={OurStory} />
+        <Route path="/OurValues" component={OurValuesInAction} />
+        <Route path="/FixingIssues" component={FixingCriticalIssues} />
+        <Route path="/Solutions" component={CommercialAndResidentialSolutions} />
+        <Route path="/Careers" component={Careers} />
+        <Route path="/ContactUs" component={ContactUs} />
+        <Route component={Notfound} />
+      </Switch>
+      <Footer/>
+    </div>
+  </Router>
+)
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
