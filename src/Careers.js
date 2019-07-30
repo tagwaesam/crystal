4fe909e241b74a7adda1918916a43dea29e929c0
Tagwa-Esam <tagwaesam5@gmail.com>
2019-07-30 10:54:46 -0400

    Create Careers.js
    add text

---
+++ src/Careers.js
@@ -0,0 +1,121 @@
+import React, { Component } from 'react';
+
+
+
+
+class Careers extends Component{
+	render() {
+		return (
+
+			<div className="CareersContainer">
+				<h1>Careers</h1>
+				<br />
+				<br />
+				<p>
+				Currently, we have one open position! (Senior Mechanical Engineer)<br /><br />
+
+				If you are interested, please contact us.<br /><br />
+				</p>
+				<p className="bold">OPEN POSITIONS:</p><br />
+				<p>
+				Job title: Energy Engineer III (PE &  CEM Required)<br /><br />
+				Job Location: Charlotte, NC, USA<br /><br />
+				Job Category: Engineering<br /><br />
+				Employment Type: Full-Time<br /><br />
+				Compensation: 100K – 140K Annually (plus commission)<br /><br />
+				Benefits: Medical Insurance<br /><br />
+				</p>
+
+				<p className="bold">Job description and duties:</p><br /><br />
+				<p>
+				• This position is for an Energy Engineer to play a leadership role
+				in Crystal House project development and implementation team.<br /><br />
+				This position will be located in the greater Dallas
+				area.<br /><br />
+				• Analyzes, validates, and evaluates complex energy and facility
+				performance data.Benchmarks energy performance against comparable buildings.<br /><br />
+				• Acts as team lead for building energy audits, conducts energy and
+				sustainability analyses, and identifies opportunities to increase/improve
+				building efficiencies and performance.<br /><br />
+				• Inspects and monitors building systems to determine energy use, identify
+				energy savings opportunities, and quantify savings potential.<br /><br />
+				• Prepares written analyses and presents recommendations for building improvements.<br /><br />
+				• Prepares design documents and project specifications.<br /><br />
+				• Develops administrative and operational plans, project budgets,
+				and implementation timelines in conjunction with the management team.<br /><br />
+				• Manages teams of contractors and in-house staff to ensure timely execution of energy
+				projects in accordance with project specifications and budgets. Reviews and approves
+				contractor payments.<br /><br />
+				• Visits project job sites and review contractor job progress and quality of work.<br /><br />
+				• Communicates with a broad team of stakeholders; provides specialized reports,
+				 serves as a technical expert, and trains clients’ personnel on proper operation
+				 and maintenance of equipment.<br /><br />
+				• Measures and verifies utility savings associated with completed projects.<br /><br />
+				• Acts as a subject matter expert on topics related to energy conservation, sustainability,
+				 and building technologies.<br /><br />
+				• Participates in and directs research and evaluations of new energy efficiency approaches,
+				measures, practices, and equipment.<br /><br />
+				• Suggests and implements management solutions to address new or ongoing problems
+				within the project development and implementation team.<br /><br />
+				• Exercise direct supervision over assigned staff and hired contractors. Trains staff as
+				necessary.<br /><br />
+				• Performs related duties and responsibilities as required.<br /><br />
+				</p>
+
+				<p className="bold">Required Qualifications:</p><br /><br />
+				<p>
+				• Bachelor’s Degree from an accredited college or university in engineering, physical
+				sciences, architecture, or a related field.<br /><br />
+				• At least five (5) years increasingly responsible experience in energy management or energy
+				engineering field, including at least two (2) years of supervisory experience.<br /><br />
+				• License and certification:<br /><br />
+				o Licensed Professional Engineer (P.E.)<br /><br />
+				o Certified Energy Manager (CEM)<br /><br />
+				• Knowledge of building energy systems, including: heating, ventilating, and air conditioning
+				(HVAC); HVAC controls; refrigeration; lighting and lighting controls; building envelope;
+				 motors; domestic hot water; and process loads.<br /><br />
+				• Knowledge of energy management practices.<br /><br />
+				• Knowledge of energy audit practices and instrumentation.<br /><br />
+				• Skilled in implementing, coordinating, and leading multifaceted energy efficiency and/or
+				conservation projects for large commercial or public buildings.<br /><br />
+				• Ability to communicate clearly and effectively, both verbally and in writing.<br /><br />
+				</p>
+
+				<p className="bold">Desired Qualifications:</p><br /><br />
+				<p>
+				•Master’sDegree.<br /><br />
+				•License, certifications, and accreditation:<br /><br />
+				o Certified Commissioning Professional (CCP)<br /><br />
+				o Building Commissioning Professional (BCxP)<br /><br />
+				o Building Energy Assessment Professional (BEAP)<br /><br />
+				o Certified Measurement and Verification Professional (CMVP)<br /><br />
+				o Certified Energy Auditor (CEA)<br /><br />
+				o Building Energy Modeling Professional (BEMP)<br /><br />
+				o Leadership in Energy and Environmental Design Accredited Professional (LEED AP)<br /><br />
+				• Ability to manage multiple building upgrade projects concurrently.<br /><br />
+				• Knowledge of professional service and construction contracts.<br /><br />
+				• Energy modeling experience.<br /><br />
+				• Skilled in project scheduling.<br /><br />
+				• Skilled in quantitative analysis methodology to analyze energy use, energy rates, and utility
+				bill data.<br /><br />
+				• Skilled in life cycle cost analysis for energy efficiency measures.<br /><br />
+				• Skilled in facility energy performance bench-marking.<br /><br />
+				• Skilled in use of computer applications and software associated with the preparation of plans
+				 and engineering computations.
+				• Ability to interpret and apply Federal, State and local policies, procedures, laws, and
+				regulations.<br /><br />
+				</p>
+
+				<p className="bold">Compensation &  Benefits:</p><br /><br />
+				<p>
+				The compensation for this position includes salary commensurate with experience, paid
+				holidays and vacation, and health insurance.<br /><br />
+				</p>
+    	</div>
+
+	 	);}
+
+}
+
+
+export default Careers;
