2ab35356482f0edef03f95f1ed263b37ec5659e1
Tagwa-Esam <tagwaesam5@gmail.com>
2019-07-30 10:57:34 -0400

    Create OurStory.js
    add text

---
+++ src/OurStory.js
@@ -0,0 +1,86 @@
+
+
+import React, { Component } from 'react';
+
+
+
+
+class OurStory extends Component{
+	render() {
+		return (
+
+			<div className="StoryContainer">
+
+				<h1>Our Story</h1>
+				<p className="P1">
+
+				• Crystal House Consultants, LLC is an energy conservation firm registered through the North
+				Carolina Board of Professional Engineers. <br />
+				• Our company was founded to provide energy conservation and project management services to
+				industrial and commercial customers.  <br />
+				• We have established a track record of successful projects yielding quantitative savings. <br />
+				• Our customers typically have a 40% to 55% reduction in their utility and maintenance costs
+				 within a reasonably short-period of time.
+				 </p>
+
+				<p className="P2">
+				• In addition to our qualifications and professional standards, Crystal House Innovations has
+				developed key management philosophies, procedures, and risk mitigation strategies that
+				govern our approach to all of our projects. <br />
+				• Crystal House Consultants, LLC is a North Carolina limited liability corporation which has
+				been continuously operating under its current name since its establishment and has not been
+				known by any other business name. <br />
+				• Crystal House is not operating under a limited partnership and will assume full responsibility
+				 for the services provided in the proposed project.
+				</p>
+				<p className="P3">
+				• Our firm has never been a defendant under litigation, nor has Crystal House ever had
+				a contract terminated for non-performance. Crystal House is certified as a Small Business
+				Enterprise(SBE) and HUB certified.
+				</p>
+				<p className="P4">
+				• Our company is committed to providing other small and disadvantaged businesses with economic opportunities. <br />
+				• Crystal House personnel hold key certifications and licensees relevant to execution of energy
+				efficiency projects.  <br />
+				• Our work will be conducted under close supervision of a Professional
+				Engineer(P.E.) licensed. Our project management team also includes numerous Certified
+				Energy Managers(CEM), certified through the Association of Energy Engineers(AEE).
+				Additional specialized certifications relevant to this project include Certified Measurement and
+
+				Verification Professional(CMVP) through AEE and Lean Six Sigma Green Belt(LSSGB)
+				through Six Sigma U.S.
+				</p>
+				<p className="P5">
+				• Crystal House concentrates on the most complex and profound challenges facing the world in
+				the process, industrial, commercial and residential markets. We refocused our business
+				portfolio to deliver solutions that bring greater value to our customers.<br/> Learn how we've
+				transformed and why:
+
+				</p>
+				<p className="P6">
+			 • Crystal House has a diverse team of highly qualified engineers with extensive experience in
+			 leading and executing energy conservation and auditing projects.  <br />
+			 • Our experience include baselines and energy use analysis for a broad range of facilities including:
+			 </p>
+			 <p className="P7">
+
+				 <ul className="list-group">
+					 <li className="list-group-item">Government facilities</li>
+					 <li className="list-group-item">Public buildings</li>
+					 <li className="list-group-item">Office buildings</li>
+					 <li className="list-group-item">Health care facilities</li>
+					 <li className="list-group-item">Commercial buildings</li>
+					 <li className="list-group-item">Parking garages</li>
+					 <li className="list-group-item">Hotels</li>
+					 <li className="list-group-item">Manufacturing facilities</li>
+					 <li className="list-group-item">Office buildings</li>
+				 </ul>
+			 </p>
+    	</div>
+
+	 	);}
+
+}
+
+
+export default OurStory;
