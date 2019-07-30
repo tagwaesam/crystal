70dc6fe0363858f9cc7b517b6a1abcf5633f2465
Tagwa-Esam <tagwaesam5@gmail.com>
2019-07-30 10:54:40 -0400

    Create CommercialAndResidentialSolutions.js
    add text

---
+++ src/CommercialAndResidentialSolutions.js
@@ -0,0 +1,31 @@
+
+
+
+import React, { Component } from 'react';
+
+
+
+
+class CommercialAndResidentialSolutions extends Component{
+	render() {
+		return (
+
+			<div className="solutionContainer">
+
+				<h1>Commercial &Residential Solutions</h1>
+				<p>
+				We are dedicated to improving the quality of lives around the world. <br /><br />
+				 Ensuring human comfort and health<br /><br />
+				 Protecting food quality and sustainability <br /><br />
+				 Advancing energy efficiency and environmental conservation<br /><br />
+				 Creating sustainable infrastructure<br /><br />
+				 Continuing momentum through our Innovation Network<br /><br />
+				</p>
+    	</div>
+
+	 	);}
+
+}
+
+
+export default CommercialAndResidentialSolutions;
